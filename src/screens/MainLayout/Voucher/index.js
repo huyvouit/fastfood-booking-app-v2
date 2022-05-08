7e9ce3bef6b50b09f36e88ea858fc6c81566d458
kimy1506 <19522552@gm.uit.edu.vn>
2022-05-08 16:36:40 +0700

    add voucher

--- src/screens/MainLayout/Voucher/index.js
+++ src/screens/MainLayout/Voucher/index.js
@@ -1,12 +1,45 @@
 import React, {useEffect} from 'react';
 import {View, Text, Image} from 'react-native';
+import {ImageBackground, ScrollView, TouchableOpacity, Button} from 'react-native';
 
 import styles from './styles';
 
+import Pizza from '../../../assets/images/Pizza.png';
+import Logo from '../../../assets/images/logo.png';
+
+
 const VoucherScreen = () => {
   return (
-    <View style={styles.root}>
-      <Text>Vouher</Text>
+    <View style={styles.container}>
+      <ImageBackground source={Pizza} resizeMode="cover" style={styles.image}>
+      </ImageBackground>
+
+      <View style={styles.tab}>
+        <TouchableOpacity style={styles.tab_AllVouchers}>
+          <Text style={styles.tab_FoodItems_Text}>All vouchers</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.tab_Used}>
+          <Text style={styles.tab_Resturents_Text}>Used</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.tab_Used}>
+          <Text style={styles.tab_Resturents_Text}>Expired</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.vouchers}>
+        <View style={styles.items}>
+          <Image style={styles.img_vouchers} source={Logo} />  
+          <View style={styles.text_vouchers}>
+            <Text style={styles.text_vouchers_id}>Mã voucher: VC001</Text>
+            <Text style={styles.text_vouchers_per}>Giảm 10%</Text>
+            <Text style={styles.text_vouchers_date}>HSD: 10/06/2022</Text>
+          </View>
+          <View style={{top: 70, left: 20}}>
+            <Button title='Save'/>
+          </View>
+        </View>
+      </View>
+
     </View>
   );
 };
