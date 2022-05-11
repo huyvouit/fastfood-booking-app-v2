db3a99d5003c11c7c87ddf51cdfff8da8ea28615
Linh <vuquanglinh2122001@gmail.com>
2022-05-12 00:02:44 +0700

    update Home

--- src/screens/MainLayout/Home/styles.js
+++ src/screens/MainLayout/Home/styles.js
@@ -91,6 +91,125 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     flexWrap: 'wrap',
     paddingHorizontal: 20,
+    marginBottom: 20,
+  },
+  HappyDeals: {
+    marginTop: 20,
+    paddingHorizontal: 20,
+  },
+  note: {
+    fontFamily: 'Roboto-Regular',
+    fontSize: 16,
+    paddingBottom: 10,
+  },
+  mainItem: {
+    flexDirection: 'row',
+    backgroundColor: '#FB6A70',
+    borderRadius: 25,
+    height: 200,
+    width: '100%',
+  },
+  text: {
+    paddingTop: 20,
+    paddingHorizontal: 15,
+    height: 100,
+    width: '50%',
+  },
+  text_2: {
+    paddingTop: 10,
+    paddingHorizontal: 15,
+  },
+  textTitle: {
+    color: '#fff',
+    fontFamily: 'Roboto-Bold',
+    fontSize: 20,
+  },
+  textNote: {
+    color: '#fff',
+    fontFamily: 'Roboto-Regular',
+    fontSize: 12,
+    fontStyle: 'italic',
+  },
+  textContent: {
+    color: '#fff',
+    fontFamily: 'Roboto-Regular',
+
+  },
+  image: {
+    width: '40%',
+    height: 100,
+  },
+  mainImage: {
+    width: 180,
+    height: 180,
+    resizeMode: 'cover',
+    position: 'relative',
+    marginTop: 20,
+    left: -30,
+    top: -15,
+  },
+  ortherItem: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginTop: 20,
+    height: 200,
+    width: '100%',
+  },
+  ortherItem_1: {
+    backgroundColor: '#DC143C',
+    height: 160,
+    width: '47%',
+    borderRadius: 25,
+    marginRight: 10,
+    overflow: 'hidden',
+  },
+  ortherItem_2: {
+    backgroundColor: '#FF00FF',
+    height: 160,
+    width: '47%',
+    borderRadius: 25,
+    marginLeft: 10,
+    overflow: 'hidden',
+  },
+  otherImage: {
+    width: 100,
+    height: 100,
+    position: 'absolute',
+    alignSelf: 'flex-end',
+    top: 100,
+  },
+  voucher: {
+    paddingHorizontal: 20,
+  },
+
+
+  gridView: {
+    marginTop: 10,
+    flex: 1,
+  },
+  itemContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 30,
+    padding: 10,
+    height: 150,
+  },
+  imageVoucher: {
+    justifyContent: 'flex-start',
+    borderRadius: 40,
+    width: 50,
+    height: 50,
+  },
+  itemName: {
+    fontSize: 18,
+    color: '#fff',
+    fontFamily: 'Roboto-Bold',
+    fontWeight: '700',
+  },
+  itemCode: {
+    fontWeight: '600',
+    fontSize: 12,
+    color: '#fff',
   },
 });
 
